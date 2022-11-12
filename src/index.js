bce2c9bdd4f8f5c8130302984b6da715d709123a
Ihor <itotonk1@gmail.com>
2022-11-12 22:47:09 +0200

    update modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+const toTopBtn = document.querySelector('.scroll-to-top');
 
+toTopBtn.addEventListener('click', e => {
+  window.scrollTo(0, 0);
+});
+
+(() => {
+  const refs = {
+    mainBtn: document.querySelector('.main__consultation-button'),
+    aboutBtn: document.querySelector('.about__consultation-button'),
+    portfBtn: document.querySelector('.portfolio__consultation-button'),
+    constBtn: document.querySelector('.contacts__consultation-button'),
+    // openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.mainBtn.addEventListener('click', toggleModal);
+  refs.aboutBtn.addEventListener('click', toggleModal);
+  refs.portfBtn.addEventListener('click', toggleModal);
+  refs.constBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
